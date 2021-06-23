25f49e8d51b6def9c867e68a3fa61cd72c1eff44
jvdhtm <jvdhtm@gmail.com>
2021-06-23 22:58:51 +0200

    adding some comments

--- packages/ProrenataSDK/src/index.tsx
+++ packages/ProrenataSDK/src/index.tsx
@@ -1,6 +1,18 @@
 import * as openapi from "openapi-typescript";
 const openapiTS =  openapi.default;
 
-export default function say(string_to_say) {
-    return console.log(string_to_say);
+
+
+/* creating a global library sdk that could be use by app developer around the world to work with tight coupling */
+
+export const ChooseModels = function (schemas : string[]) {
+    return console.log(schemas);
+};
+
+export const SyncModel = function (template : string) {
+    return console.log(template);
+};
+
+export default function Create(template : string) {
+    return console.log(template);
 };
